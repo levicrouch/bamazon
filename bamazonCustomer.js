20cd0f0ed37b2e3dc6fe119ba9322030bec6132d
Levi Crouch <levi.crouch@gmail.com>
2018-01-08 00:13:39 -0800

    saving work

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,63 +1,117 @@
 var mySQL = require("mySQL");
 var inquirer = require("inquirer");
+var arrItems = [];
 
-function getCreds(creds) {
-    // prompt for username and password to the users MySQL instance
-    if (!creds) {
-        inquirer.prompt([{
-            name: "username",
-            type: "input",
-            message: "Please enter a username to connect to your local MySQL instance"
-        },
-        {
-            name: "password",
-            type: "password",
-            message: "Please enter the password associated with the account to access your local MySQL instance"
-        }]).then(function (answer) {
-            // if 'local' is entered, then use my credentials to connect to db
-            if (answer.username === "local") {
-                console.log("You entered local as your username");
-                var connection = mySQL.createConnection({
-                    host: "localhost",
-                    port: 3306,
-                    // local username
-                    user: "dbuser",
-                    // Your password
-                    password: "SecureSQLPw%1",
-                    database: "bamazon"
-                });
-            } else {
-                // use the credentials captured by inquirer to connect to the local MySQL instance 
-                var connection = mySQL.createConnection({
-                    host: "localhost",
-                    port: 3306,
-                    // local username
-                    user: answer.username,
-                    // Your password
-                    password: answer.password,
-                    database: "bamazon"
-                });
+// var objCreds = {
+//     getCreds: function () {
+//         // function to grab username
+// inquirer.prompt([{
+//     name: "username",
+//     type: "input",
+//     message: "Please enter a username to connect to your local MySQL instance"
+// },
+// {
+//     name: "password",
+//     type: "input",
+//     message: "Please enter a password to connect to your local MySQL instance"
+// }]).then(function (answer) {
+//     if (answer.username === "local") {
+//         // if local is entered, uses my local credentials
+//         console.log("You entered local as your username");
+//         objCreds = {
+//             username: "dbuser",
+//             password: "SecureSQLPw%1",
+//         }
+//     } else {
+//         objCreds = {
+//             username: answer.username,
+//             password: answer.password,
+//         }
+//     }
+// });
+//     },
+// }
+// objCreds.getCreds();
+// console.log("Username:", objCreds.getUsername());
+var connection = mySQL.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "dbuser",
+    password: "SecureSQLPw%1",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to MySQL");
+    // connection.end();
+    // console.log("dis-connected from MySQL");
+    startUp();
+});
+
+// function listItems() {
+//     // connect to SQL and grab all items
+//     connection.connect(function (err) {
+//         if (err) throw err;
+//         var sql = "SELECT * FROM products";
+//         connection.query(sql, function (err, res) {
+//             console.log("---------------------------------");
+//             for (var i = 0; i < res.length; i++) {
+//                 console.log("Item ID:", res[i].item_id +
+//                     " | " + "Product:", res[i].product_name +
+//                     " | " + "Department:", res[i].department_name +
+//                     " | " + "Price:", res[i].price +
+//                     " | " + "Stock:", res[i].stock_quantity
+//                 );
+//                 // push the db items into an array
+//                 arrItems.push(res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity);
+//             }
+//             console.log("---------------------------------");
+//             // close the connection
+//             connection.end();
+//         });
+//     });
+// }
+
+function startUp() {
+    var sql = "SELECT * FROM products";
+    connection.query(sql, function (err, res) {
+        if (err) throw err;
+        // console.log("---------------------------------");
+        // for (var i = 0; i < res.length; i++) {
+        //     console.log("Item ID:", res[i].item_id +
+        //         " | " + "Product:", res[i].product_name +
+        //         " | " + "Department:", res[i].department_name +
+        //         " | " + "Price:", res[i].price +
+        //         " | " + "Stock:", res[i].stock_quantity
+        //     );
+        // }
+        // console.log("---------------------------------");
+        inquirer.prompt([
+            {
+                name: "choice",
+                type: "rawlist",
+                choices: function () {
+                    var arrItems = [];
+                    for (var i = 0; i < res.length; i++) {
+                        // push the db items into an array
+                        // arrItems.push(res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity);
+                        arrItems.push(res[i].item_id);
+                    }
+                    return arrItems;
+                },
+                // choices: ["1", "2"],
+                message: "Welcome to Bamazon! Take a look at the selections below and choose an item to buy",
+
+            },
+            {
+                name: "bid",
+                type: "input",
+                message: "How much would you like to bid?"
             }
-            if (answer.username && answer.password) {
-                // connect to SQL
-                connection.connect(function (err) {
-                    if (err) throw err;
-                    // return all rows to the console
-                    var sql = "SELECT * FROM products";
-                    connection.query(sql, function (err, res) {
-                        console.log("---------------------------------");
-                        for (var i = 0; i < res.length;i++){
-                            console.log("Product:", res[i].product_name + 
-                            " | " + "Department:", res[i].department_name +
-                            " | " + "Price:", res[i].price +
-                            " | " + "Stock:", res[i].stock_quantity
-                        );
-                        }
-                        console.log("---------------------------------");
-                    });
-                });
-            }
-        });
-    }
+        ]).then(function (response) {
+            // case 
+            console.log("You chose:", response);
+        })
+    });
 }
-getCreds(false);
